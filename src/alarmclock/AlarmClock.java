a25919f3373c770a7b5ff895509241688c390163
Christopher <christophervalle3338@gmail.com>
2025-01-14 15:22:52 -0800

    started the project with asking user with alarm time, storing it and displaying it

--- src/alarmclock/AlarmClock.java
+++ src/alarmclock/AlarmClock.java
@@ -1,10 +1,20 @@
 package alarmclock;
 
+import java.util.Scanner;
+
 public class AlarmClock {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scnr = new Scanner(System.in);
+		
+		//asking user for time for alarm
+		System.out.print("Please input the time you want the alarm clock (11:29): ");
+		String alarmTime = scnr.nextLine(); //storing alarmtime as a string
+		
+		//displaying users alarm time
+		System.out.println("Your alarm will ring at " + alarmTime); 
+		
+		scnr.close(); //close scanner avoiding resource leak
 	}
 
 }
